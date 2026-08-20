d11cedbab7af5cbc00348baead3af37e0ba07ae9
jorgebueno-u <nvgjorgeme@gmail.com>
2026-08-19 19:53:23 -0500

    Add to_dict and from_dict methods to product class

--- product.py
+++ product.py
@@ -18,6 +18,25 @@ class Product:
 
         self.price = price
 
+    def to_dict(self):
+        return {
+            "product_id": self.product_id,
+            "name": self.name,
+            "category": self.category,
+            "price": self.price,
+            "quantity": self.quantity
+        }
+
+    @classmethod
+    def from_dict(cls, data):
+        return cls(
+            data["product_id"],
+            data["name"],
+            data["category"],
+            data["price"],
+            data["quantity"]
+        )
+
     def __str__(self):
         return (
             f"ID: {self.product_id} | "
